2aff28d98d157feb12bdc78523978af2185f9b4c
Ledion Restelica <ledionrestelica@icloud.com>
2025-09-14 20:08:38 +0200

    Add Chat link to user info card header
    A 'Chat' link was added to the CardHeader in the UserInfo component to provide quick navigation to the chat page.

--- components/auth/user-info.tsx
+++ components/auth/user-info.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { Card, CardTitle, CardHeader, CardContent } from "../ui/card";
 import { SignOutButton } from "./sign-out-button";
+import Link from "next/link";
 
 export default async function UserInfo() {
   const session = await auth.api.getSession({
@@ -11,6 +12,7 @@ export default async function UserInfo() {
     <Card className="max-w-[500px]">
       <CardHeader className="flex items-center justify-between">
         <CardTitle>User Info</CardTitle>
+        <Link href="/chat">Chat</Link>
         <SignOutButton />
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
