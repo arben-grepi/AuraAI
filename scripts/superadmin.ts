120f7d0fcfb2260bf15857a8cfba26954a4d9bda
Ledion Restelica <135837524+Ledionrestelica@users.noreply.github.com>
2026-02-28 16:58:30 +0100

    Fix

--- scripts/superadmin.ts
+++ scripts/superadmin.ts
@@ -1,7 +1,7 @@
 import prisma from "@/lib/prisma";
 
 async function makeSuperAdmin() {
-  const emails = ["ledi@gmail.com"];
+  const emails = ["ledionrestelica7@gmail.com"];
 
   const users = await prisma.user.findMany({
     where: {
